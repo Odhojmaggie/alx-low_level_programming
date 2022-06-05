d94983d3c4f0a44d4598e7299dbc16e6e987567f
odhojmaggie <maggie.odhoj@gmail.com>
2022-06-05 05:13:21 -0700

    :sunny: A program that prints a message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - using the function puts on this program to display a message
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
