f024e2b5efb1e27df10010a1906dfb7d7745eecb
odhojmaggie <maggie.odhoj@gmail.com>
2022-06-06 10:58:09 -0700

    Possible combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints a combination of numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int c, i;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(c);
+			putchar(i);
+
+			if (c != '8' || (c == '8' && i != '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
